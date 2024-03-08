8c8f6dc6e166f6b48d06f6b5b74b648f8cbfab9a
= <=>
2024-03-08 09:53:06 +0530

    added base_blur.html

--- code_connect/home/models.py
+++ code_connect/home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Member(models.Model):
+    pass
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+
+    # phone, department (Choices or Foreign Key), roll num
+    # 
--- code_connect/home/urls.py
+++ code_connect/home/urls.py
@@ -5,4 +5,5 @@ app_name = "home"
 
 urlpatterns = [
 	path("", views.index, name="index"),
+    path("member/registration/", views.register, name="member_registration"),
 ]
